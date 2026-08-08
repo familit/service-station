abdd24c06878495b77b925c34b22c48478a58bee
Krapfeen <theAndrewSchaefer@gmail.com>
2026-08-09 00:54:59 +0500

    feat: added Sidebar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark min-vh-100 w-100">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+      <span class="fs-4">service-station</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li><a href="#" class="nav-link text-white">Карточка клиента</a></li>
+      <li><a href="#" class="nav-link text-white">Карточка автомобиля</a></li>
+      <li><a href="#" class="nav-link text-white">Заказ-наряды</a></li>
+      <li><a href="#" class="nav-link text-white">Календарь</a></li>
+    </ul>
+    <hr>
+    <a href="/" class="nav-link active" aria-current="page">Выйти из клиента</a>
+  </div>
+</template>
