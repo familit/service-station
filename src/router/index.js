2c123432b204b0466dfdb40f2207dfc86c664b08
Krapfeen <theAndrewSchaefer@gmail.com>
2026-08-09 01:00:05 +0500

    feat: added page of client

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import {createRouter, createWebHashHistory } from 'vue-router'
 
 import SearchView from "../views/SearchView.vue";
+import ClientCardView from "../views/ClientCardView.vue";
 
 const routes = [
     { path: '/', component: SearchView },
+    { path: '/client', component: ClientCardView }
 ]
 
 export const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHashHistory(),
     routes,
 })
---
+++ src/views/ClientCardView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Sidebar from "../components/Sidebar.vue";
+import ClientCard from "../components/ClientCard.vue";
+import AutoCard from "../components/AutoCard.vue";
+import WorkOrderCard from "../components/WorkOrderCard.vue";
+</script>
+
+<template>
+  <main class="container-fluid w-100 d-flex flex-wrap p-0">
+    <div class="row w-100">
+      <div class="col-sm-3">
+        <Sidebar />
+      </div>
+      <div class="col-sm-9 bg-secondary d-flex justify-content-center align-items-center flex-column gap-5">
+        <ClientCard />
+        <AutoCard />
+        <WorkOrderCard />
+      </div>
+    </div>
+  </main>
+</template>
