f261fbf0803101582689485e5e381fe70713f672
Krapfeen <theAndrewSchaefer@gmail.com>
2026-08-10 20:24:51 +0500

    feat: added page of work order

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import {createRouter, createWebHashHistory } from 'vue-router'
 import SearchView from "../views/SearchView.vue";
 import ClientCardView from "../views/ClientCardView.vue";
 import CarsView from "../views/CarsView.vue";
+import WorkOrderView from "../views/WorkOrderView.vue";
 
 const routes = [
     { path: '/', component: SearchView },
     { path: '/client', component: ClientCardView },
     { path: '/car', component: CarsView },
+    { path: '/work-order', component: WorkOrderView },
 ]
 
 export const router = createRouter({
