ab9fba77441c49ddd65143773c350d4a65dce90b
Krapfeen <theAndrewSchaefer@gmail.com>
2026-08-10 20:14:51 +0500

    feat: added page of car

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import {createRouter, createWebHashHistory } from 'vue-router'
 
 import SearchView from "../views/SearchView.vue";
 import ClientCardView from "../views/ClientCardView.vue";
+import CarsView from "../views/CarsView.vue";
 
 const routes = [
     { path: '/', component: SearchView },
-    { path: '/client', component: ClientCardView }
+    { path: '/client', component: ClientCardView },
+    { path: '/car', component: CarsView },
 ]
 
 export const router = createRouter({
