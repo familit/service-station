2526c48fb0031434efa3d7435630f034caddf333
Krapfeen <theAndrewSchaefer@gmail.com>
2026-08-14 16:08:00 +0500

    style: added code of contry to input

--- src/components/Input/PhoneInput.vue
+++ src/components/Input/PhoneInput.vue
@@ -27,6 +27,7 @@
 <template>
   <div class="input-group">
     <span class="input-group-text">{{ label }}</span>
+    <span class="input-group-text">+7</span>
     <input class="form-control" type="number" :value="value" :id="id" :name="id" :placeholder="placeholder" :readonly :required min="9000000000" max="9999999999">
     <a v-if="!required" :href="'tel:+7'+value" class="btn btn-outline-secondary"><i class="bi bi-telephone-fill"></i></a>
   </div>
